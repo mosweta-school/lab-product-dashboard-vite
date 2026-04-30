3e864c22d5f83aff54ebe44fb72f0ceebf3fe3c0
mosweta-school <deograciousmoriasi10@gmail.com>
2026-04-30 14:58:39 +0300

    second commit

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,6 +1,7 @@
 import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
+import 'jsdom' // Required for CodeGrade
 import 'whatwg-fetch' // Required for CodeGrade
 import { Blob } from 'node:buffer' // Fix for Node 16 issues
 
